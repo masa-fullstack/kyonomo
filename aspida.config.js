73cc8bc1caa0303c0bceed30ca855bd00ea68103
masa-fullstack <ishida.masahidee@gmail.com>
2021-06-27 00:53:06 +0900

    initial commit

--- aspida.config.js
+++ aspida.config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './.env.local' })
 
-module.exports = { input: 'src/api', baseURL: `${process.env.NEXT_PUBLIC_MY_URL}/api` }
+module.exports = { input: 'src/api', baseURL: `${process.env.NEXT_PUBLIC_SITE_URL}/api` }
