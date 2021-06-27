b164d019bc12fb86cf48b8ed04c368572daf6987
masa-fullstack <ishida.masahidee@gmail.com>
2021-06-27 21:14:30 +0900

    initial commit

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 import { Footer } from '../Footer'
 
-const Component: React.FC = ({ children }) => (
+type Props = {
+  children?: React.ReactNode
+}
+
+const Component: React.VFC<Props> = ({ children }) => (
   <>
     <main className="h-screen pb-10 pt-4 md:pt-10 container mx-auto px-4 flex justify-center">{children}</main>
     <div className="absolute bottom-0 h-10">
@@ -11,8 +15,8 @@ const Component: React.FC = ({ children }) => (
   </>
 )
 
-const Container: React.FC = () => {
-  return <Component />
+const Container: React.VFC<Props> = (props) => {
+  return <Component {...props} />
 }
 
 export default Container
