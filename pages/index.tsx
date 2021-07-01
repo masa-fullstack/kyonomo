8d3f3b351887b3fe8cef504cb79b5099665c1807
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-01 20:45:34 +0900

    lifffixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 
 import { InvitationDetail } from '~/src/components/InvitationDetail'
 import { Layout } from '~/src/components/Layout'
-import { useLiff } from '~/src/components/hooks/useLiff'
+// import { useLiff } from '~/src/components/hooks/useLiff'
 import { OG_TITLE, returnTitle } from '~/src/utils/meta'
 
 const IndexPage = async () => {
   const title = returnTitle()
-  const { userId } = await useLiff()
-
+  // const { userId } = await useLiff()
+  const { userId } = { userId: 'test' }
   return (
     <>
       <Head>
