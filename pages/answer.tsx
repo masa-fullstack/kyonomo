5ebff8f49fb31e27665802dd4afac08037d43622
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-02 17:26:09 +0900

    liff

--- pages/answer.tsx
+++ pages/answer.tsx
@@ -3,16 +3,23 @@ import Head from 'next/head'
 
 import { AnswerDetail } from '~/src/components/AnswerDetail'
 import { Layout } from '~/src/components/Layout'
-import { OG_TITLE, returnTitle } from '~/src/utils/meta'
+import { OG_TITLE, DESCRIPTION, OG_DESCRIPTION, OG_IMAGE, returnTitle } from '~/src/utils/meta'
 
 const AnswerPage: NextPage = () => {
-  const title = returnTitle()
-
+  const title = returnTitle('回答する')
+  const description = 'リンク先で３秒で回答できます'
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta key={OG_TITLE} property={OG_TITLE} content={title} />
+        <meta key={DESCRIPTION} name={DESCRIPTION} content={description} />
+        <meta key={OG_DESCRIPTION} property={OG_DESCRIPTION} content={description} />
+        <meta
+          key={OG_IMAGE}
+          property={OG_IMAGE}
+          content={`${process.env.NEXT_PUBLIC_SITE_URL}/images/ANSWER_OGP.png`}
+        />
       </Head>
       <Layout>
         <AnswerDetail />
