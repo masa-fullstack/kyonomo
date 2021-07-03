9a5b60f51999c7a0faf8bc6848f775970bd4b891
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 17:28:21 +0900

    liff

--- src/components/InvitationDetail/InvitationDetail.tsx
+++ src/components/InvitationDetail/InvitationDetail.tsx
@@ -198,7 +198,101 @@ const Container: React.VFC = () => {
                 weight: 'bold',
                 size: 'xl',
               },
+              {
+                type: 'box',
+                layout: 'vertical',
+                margin: 'lg',
+                spacing: 'sm',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    spacing: 'sm',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'Place',
+                        color: '#aaaaaa',
+                        size: 'sm',
+                        flex: 1,
+                      },
+                      {
+                        type: 'text',
+                        text: 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
+                        wrap: true,
+                        color: '#666666',
+                        size: 'sm',
+                        flex: 5,
+                      },
+                    ],
+                  },
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    spacing: 'sm',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'Time',
+                        color: '#aaaaaa',
+                        size: 'sm',
+                        flex: 1,
+                      },
+                      {
+                        type: 'text',
+                        text: '10:00 - 23:00',
+                        wrap: true,
+                        color: '#666666',
+                        size: 'sm',
+                        flex: 5,
+                      },
+                    ],
+                  },
+                ],
+              },
             ],
+          },
+          footer: {
+            type: 'box',
+            layout: 'vertical',
+            spacing: 'sm',
+            contents: [
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: 'OK🍻',
+                  uri: 'https://linecorp.com',
+                },
+              },
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: 'Hmm...',
+                  uri: 'https://linecorp.com',
+                },
+              },
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: 'NG',
+                  uri: 'https://linecorp.com',
+                },
+              },
+              {
+                type: 'spacer',
+                size: 'sm',
+              },
+            ],
+            flex: 0,
           },
         },
       },
