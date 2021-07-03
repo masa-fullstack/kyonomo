1c71381f990c97147542c89f32e01e1e47dc1f31
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 15:10:13 +0900

    liff

--- src/utils/shareTargetPicker.ts
+++ src/utils/shareTargetPicker.ts
@@ -4,33 +4,123 @@ export const shareTargetPicker = async (text: string, url: string) => {
   if (liff.isApiAvailable('shareTargetPicker')) {
     liff.shareTargetPicker([
       {
-        type: 'template',
-        altText: 'This is a buttons template',
-        template: {
-          type: 'buttons',
-          // thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
-          // imageAspectRatio: 'rectangle',
-          // imageSize: 'cover',
-          // imageBackgroundColor: '#FFFFFF',
-          // title: 'Menu',
-          text: 'Please select',
-          actions: [
+        type: 'bubble',
+        hero: {
+          type: 'image',
+          url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+          size: 'full',
+          aspectRatio: '20:13',
+          aspectMode: 'cover',
+          action: {
+            type: 'uri',
+            uri: 'http://linecorp.com/',
+          },
+        },
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
             {
-              type: 'postback',
-              label: text,
-              data: 'action=buy&itemid=123',
+              type: 'text',
+              text: 'Brown Cafe',
+              weight: 'bold',
+              size: 'xl',
             },
             {
-              type: 'postback',
-              label: 'Add to cart',
-              data: 'action=add&itemid=123',
-            },
-            {
-              type: 'uri',
-              label: url,
-              uri: 'http://example.com/page/123',
+              type: 'box',
+              layout: 'vertical',
+              margin: 'lg',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'box',
+                  layout: 'baseline',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: 'Place',
+                      color: '#aaaaaa',
+                      size: 'sm',
+                      flex: 1,
+                    },
+                    {
+                      type: 'text',
+                      text: text,
+                      wrap: true,
+                      color: '#666666',
+                      size: 'sm',
+                      flex: 5,
+                    },
+                  ],
+                },
+                {
+                  type: 'box',
+                  layout: 'baseline',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: 'Time',
+                      color: '#aaaaaa',
+                      size: 'sm',
+                      flex: 1,
+                    },
+                    {
+                      type: 'text',
+                      text: '10:00 - 23:00',
+                      wrap: true,
+                      color: '#666666',
+                      size: 'sm',
+                      flex: 5,
+                    },
+                  ],
+                },
+              ],
             },
           ],
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'sm',
+          contents: [
+            {
+              type: 'button',
+              style: 'link',
+              height: 'sm',
+              action: {
+                type: 'uri',
+                label: 'OK🍻',
+                uri: url,
+              },
+            },
+            {
+              type: 'button',
+              style: 'link',
+              height: 'sm',
+              action: {
+                type: 'uri',
+                label: 'Hmm...',
+                uri: 'https://linecorp.com',
+              },
+            },
+            {
+              type: 'button',
+              style: 'link',
+              height: 'sm',
+              action: {
+                type: 'uri',
+                label: 'NG',
+                uri: 'https://linecorp.com',
+              },
+            },
+            {
+              type: 'spacer',
+              size: 'sm',
+            },
+          ],
+          flex: 0,
         },
       },
       // {
