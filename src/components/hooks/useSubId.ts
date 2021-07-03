00e07e93433d8416de009cbd0d8a2e61f5fed59b
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 21:59:05 +0900

    liff

--- src/components/hooks/useSubId.ts
+++ src/components/hooks/useSubId.ts
@@ -16,18 +16,27 @@ type SetLocalSubId = (localSubId: LocalSubId) => void
 
 const setLocalSubId: SetLocalSubId = (localSubId) => {
   const localSubIds = getLocalSubIds()
+  // eslint-disable-next-line no-console
+  console.log(`セット前localSubIds:${localSubIds}`)
+
   const updatedLocalSubIds =
     localSubIds === undefined
       ? [localSubId]
       : localSubIds.map((e) => {
           e.id === localSubId.id ? localSubId : e
         })
+  // eslint-disable-next-line no-console
+  console.log(`更新のやつupdatedLocalSubIds:${updatedLocalSubIds}`)
   localStorage.setItem('localSubIds', JSON.stringify(updatedLocalSubIds))
 }
 
 export const useLocalSubId = (id: string) => {
   const [localSubIds] = useState<LocalSubId[] | undefined>(getLocalSubIds())
+  // eslint-disable-next-line no-console
+  console.log(`最初localSubIds:${localSubIds}`)
   const localSubId = localSubIds === undefined ? undefined : localSubIds.filter((e) => e.id === id)[0]
+  // eslint-disable-next-line no-console
+  console.log(`最初撮ったlocalSubId:${localSubId}`)
 
   return {
     localSubId,
