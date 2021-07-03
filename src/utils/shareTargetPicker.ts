6c78e011c295f5374c4fb88facde9e6baaf5035e
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 16:28:05 +0900

    liff

--- src/utils/shareTargetPicker.ts
+++ src/utils/shareTargetPicker.ts
@@ -27,6 +27,58 @@ export const shareTargetPicker = async (text: string, url: string) => {
                   weight: 'bold',
                   size: 'xl',
                 },
+                {
+                  type: 'box',
+                  layout: 'vertical',
+                  margin: 'lg',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: 'Place',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1,
+                        },
+                        {
+                          type: 'text',
+                          text: text,
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5,
+                        },
+                      ],
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: 'Time',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1,
+                        },
+                        {
+                          type: 'text',
+                          text: '10:00 - 23:00',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5,
+                        },
+                      ],
+                    },
+                  ],
+                },
               ],
             },
           },
