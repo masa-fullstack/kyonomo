ba599f8e91714a5a03e5088d998a50f2fc39fffa
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 14:55:26 +0900

    liff

--- src/utils/shareTargetPicker.ts
+++ src/utils/shareTargetPicker.ts
@@ -4,9 +4,39 @@ export const shareTargetPicker = async (text: string, url: string) => {
   if (liff.isApiAvailable('shareTargetPicker')) {
     liff.shareTargetPicker([
       {
-        type: 'text',
-        text: `${text}\n${url}`,
+        type: 'template',
+        altText: 'This is a buttons template',
+        template: {
+          type: 'buttons',
+          thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+          imageAspectRatio: 'rectangle',
+          imageSize: 'cover',
+          imageBackgroundColor: '#FFFFFF',
+          title: 'Menu',
+          text: 'Please select',
+          actions: [
+            {
+              type: 'postback',
+              label: text,
+              data: 'action=buy&itemid=123',
+            },
+            {
+              type: 'postback',
+              label: 'Add to cart',
+              data: 'action=add&itemid=123',
+            },
+            {
+              type: 'uri',
+              label: url,
+              uri: 'http://example.com/page/123',
+            },
+          ],
+        },
       },
+      // {
+      //   type: 'text',
+      //   text: `${text}\n${url}`,
+      // },
     ])
   }
 }
