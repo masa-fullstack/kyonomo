b063b1695b2fa2e44a2899d458b4ebe152e986da
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-03 15:59:37 +0900

    liff

--- src/utils/shareTargetPicker.ts
+++ src/utils/shareTargetPicker.ts
@@ -8,140 +8,140 @@ export const shareTargetPicker = async (text: string, url: string) => {
           type: 'flex',
           altText: 'this is a flex message',
           contents: {
-            type: 'bubble', // ①
+            //   type: 'bubble', // ①
+            //   body: {
+            //     // ②
+            //     type: 'box', // ③
+            //     layout: 'horizontal', // ④
+            //     contents: [
+            //       // ⑤
+            //       {
+            //         type: 'text', // ⑥
+            //         text: 'Hello,',
+            //       },
+            //       {
+            //         type: 'text', // ⑥
+            //         text: 'World!',
+            //       },
+            //     ],
+            //   },
+            // },
+            // {
+            type: 'bubble',
+            // hero: {
+            //   type: 'image',
+            //   url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+            //   size: 'full',
+            //   aspectRatio: '20:13',
+            //   aspectMode: 'cover',
+            // },
             body: {
-              // ②
-              type: 'box', // ③
-              layout: 'horizontal', // ④
+              type: 'box',
+              layout: 'vertical',
               contents: [
-                // ⑤
                 {
-                  type: 'text', // ⑥
-                  text: 'Hello,',
+                  type: 'text',
+                  text: 'Brown Cafe',
+                  weight: 'bold',
+                  size: 'xl',
                 },
                 {
-                  type: 'text', // ⑥
-                  text: 'World!',
+                  type: 'box',
+                  layout: 'vertical',
+                  margin: 'lg',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: 'Place',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1,
+                        },
+                        {
+                          type: 'text',
+                          text: text,
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5,
+                        },
+                      ],
+                    },
+                    {
+                      type: 'box',
+                      layout: 'baseline',
+                      spacing: 'sm',
+                      contents: [
+                        {
+                          type: 'text',
+                          text: 'Time',
+                          color: '#aaaaaa',
+                          size: 'sm',
+                          flex: 1,
+                        },
+                        {
+                          type: 'text',
+                          text: '10:00 - 23:00',
+                          wrap: true,
+                          color: '#666666',
+                          size: 'sm',
+                          flex: 5,
+                        },
+                      ],
+                    },
+                  ],
                 },
               ],
             },
+            footer: {
+              type: 'box',
+              layout: 'vertical',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'button',
+                  style: 'link',
+                  height: 'sm',
+                  action: {
+                    type: 'uri',
+                    label: 'OK',
+                    uri: url,
+                  },
+                },
+                {
+                  type: 'button',
+                  style: 'link',
+                  height: 'sm',
+                  action: {
+                    type: 'uri',
+                    label: 'Hmm...',
+                    uri: 'https://linecorp.com',
+                  },
+                },
+                {
+                  type: 'button',
+                  style: 'link',
+                  height: 'sm',
+                  action: {
+                    type: 'uri',
+                    label: 'NG',
+                    uri: 'https://linecorp.com',
+                  },
+                },
+                {
+                  type: 'spacer',
+                  size: 'sm',
+                },
+              ],
+              flex: 0,
+            },
           },
-          // // {
-          // //   type: 'bubble',
-          // //   // hero: {
-          // //   //   type: 'image',
-          // //   //   url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-          // //   //   size: 'full',
-          // //   //   aspectRatio: '20:13',
-          // //   //   aspectMode: 'cover',
-          // //   // },
-          // //   body: {
-          // //     type: 'box',
-          // //     layout: 'vertical',
-          // //     contents: [
-          // //       {
-          // //         type: 'text',
-          // //         text: 'Brown Cafe',
-          // //         weight: 'bold',
-          // //         size: 'xl',
-          // //       },
-          // //       {
-          // //         type: 'box',
-          // //         layout: 'vertical',
-          // //         margin: 'lg',
-          // //         spacing: 'sm',
-          // //         contents: [
-          // //           {
-          // //             type: 'box',
-          // //             layout: 'baseline',
-          // //             spacing: 'sm',
-          // //             contents: [
-          // //               {
-          // //                 type: 'text',
-          // //                 text: 'Place',
-          // //                 color: '#aaaaaa',
-          // //                 size: 'sm',
-          // //                 flex: 1,
-          // //               },
-          // //               {
-          // //                 type: 'text',
-          // //                 text: text,
-          // //                 wrap: true,
-          // //                 color: '#666666',
-          // //                 size: 'sm',
-          // //                 flex: 5,
-          // //               },
-          // //             ],
-          // //           },
-          // //           {
-          // //             type: 'box',
-          // //             layout: 'baseline',
-          // //             spacing: 'sm',
-          // //             contents: [
-          // //               {
-          // //                 type: 'text',
-          // //                 text: 'Time',
-          // //                 color: '#aaaaaa',
-          // //                 size: 'sm',
-          // //                 flex: 1,
-          // //               },
-          // //               {
-          // //                 type: 'text',
-          // //                 text: '10:00 - 23:00',
-          // //                 wrap: true,
-          // //                 color: '#666666',
-          // //                 size: 'sm',
-          // //                 flex: 5,
-          // //               },
-          // //             ],
-          // //           },
-          // //         ],
-          // //       },
-          // //     ],
-          // //   },
-          // //   footer: {
-          // //     type: 'box',
-          // //     layout: 'vertical',
-          // //     spacing: 'sm',
-          // //     contents: [
-          // //       {
-          // //         type: 'button',
-          // //         style: 'link',
-          // //         height: 'sm',
-          // //         action: {
-          // //           type: 'uri',
-          // //           label: 'OK',
-          // //           uri: url,
-          // //         },
-          // //       },
-          // //       {
-          // //         type: 'button',
-          // //         style: 'link',
-          // //         height: 'sm',
-          // //         action: {
-          // //           type: 'uri',
-          // //           label: 'Hmm...',
-          // //           uri: 'https://linecorp.com',
-          // //         },
-          // //       },
-          // //       {
-          // //         type: 'button',
-          // //         style: 'link',
-          // //         height: 'sm',
-          // //         action: {
-          // //           type: 'uri',
-          // //           label: 'NG',
-          // //           uri: 'https://linecorp.com',
-          // //         },
-          // //       },
-          // //       {
-          // //         type: 'spacer',
-          // //         size: 'sm',
-          // //       },
-          // //     ],
-          // //     flex: 0,
-          // //   },
-          // },
         },
         // {
         //   type: 'template',
