b31f15dc9f47ddf2a7e991e73e8cf079389e79fb
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-05 19:34:40 +0900

    liff

--- src/utils/getMessages.ts
+++ src/utils/getMessages.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { SendMessagesParams } from '@line/liff/dist/lib/api/sendMessages'
 
 type GetMessage = (
@@ -17,182 +18,203 @@ export const getMessages: GetMessage = (id, isLiff, okURL, hmURL, ngURL, answerU
   const messages: SendMessagesParams = [
     {
       type: 'flex',
-      altText: subject,
+      altText: 'this is a flex message',
       contents: {
         type: 'bubble',
-        hero: {
-          type: 'image',
-          url: 'https://kyonomo.vercel.app/images/HEADER.png',
-          size: 'full',
-          aspectRatio: '2:1',
-          aspectMode: 'cover',
-        },
         body: {
           type: 'box',
           layout: 'vertical',
           contents: [
             {
               type: 'text',
-              text: subject,
-              wrap: true,
-              weight: 'bold',
-              size: 'xl',
-            },
-            {
-              type: 'box',
-              layout: 'vertical',
-              margin: 'lg',
-              spacing: 'sm',
-              contents: [
-                {
-                  type: 'box',
-                  layout: 'baseline',
-                  spacing: 'sm',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: 'Place',
-                      color: '#aaaaaa',
-                      size: 'sm',
-                      flex: 1,
-                    },
-                    {
-                      type: 'text',
-                      text: place,
-                      wrap: true,
-                      color: '#666666',
-                      size: 'sm',
-                      flex: 5,
-                    },
-                  ],
-                },
-                {
-                  type: 'box',
-                  layout: 'baseline',
-                  spacing: 'sm',
-                  contents: [
-                    {
-                      type: 'text',
-                      text: 'Time',
-                      color: '#aaaaaa',
-                      size: 'sm',
-                      flex: 1,
-                    },
-                    {
-                      type: 'text',
-                      text: time,
-                      wrap: true,
-                      color: '#666666',
-                      size: 'sm',
-                      flex: 5,
-                    },
-                  ],
-                },
-              ],
+              text: 'hello',
             },
             {
               type: 'text',
-              text: text,
-              wrap: true,
-              margin: 'md',
-              size: 'sm',
+              text: 'world',
             },
           ],
-        },
-        footer: {
-          type: 'box',
-          layout: 'vertical',
-          spacing: 'xxl',
-          contents: [
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'uri',
-                    label: 'OK🍻',
-                    uri: `${okURL}?id=${id}&isLiff=${isLiff}`,
-                  },
-                  color: '#FFFFFF',
-                },
-              ],
-              backgroundColor: '#3b82f6',
-              cornerRadius: 'xl',
-              paddingAll: 'md',
-            },
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'uri',
-                    label: 'Hmm...🤔',
-                    uri: `${hmURL}?id=${id}&isLiff=${isLiff}`,
-                  },
-                  color: '#FFFFFF',
-                },
-              ],
-              backgroundColor: '#fbbf24',
-              cornerRadius: 'xl',
-              paddingAll: 'md',
-            },
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'uri',
-                    label: 'NG🙅‍♂️',
-                    uri: `${ngURL}?id=${id}&isLiff=${isLiff}`,
-                  },
-                  color: '#FFFFFF',
-                },
-              ],
-              backgroundColor: '#f87171',
-              paddingAll: 'md',
-              cornerRadius: 'xl',
-            },
-            {
-              type: 'box',
-              layout: 'vertical',
-              contents: [
-                {
-                  type: 'button',
-                  style: 'link',
-                  height: 'sm',
-                  action: {
-                    type: 'uri',
-                    label: '回答確認',
-                    uri: `${answerURL}?id=${id}&isLiff=${isLiff}`,
-                  },
-                  color: '#000000',
-                },
-              ],
-              backgroundColor: '#dddddd',
-              paddingAll: 'md',
-              cornerRadius: 'xl',
-            },
-            {
-              type: 'spacer',
-              size: 'sm',
-            },
-          ],
-          flex: 0,
         },
       },
     },
+    // {
+    //   type: 'flex',
+    //   altText: subject,
+    //   contents: {
+    //     type: 'bubble',
+    //     hero: {
+    //       type: 'image',
+    //       url: 'https://kyonomo.vercel.app/images/HEADER.png',
+    //       size: 'full',
+    //       aspectRatio: '2:1',
+    //       aspectMode: 'cover',
+    //     },
+    //     body: {
+    //       type: 'box',
+    //       layout: 'vertical',
+    //       contents: [
+    //         {
+    //           type: 'text',
+    //           text: subject,
+    //           wrap: true,
+    //           weight: 'bold',
+    //           size: 'xl',
+    //         },
+    //         {
+    //           type: 'box',
+    //           layout: 'vertical',
+    //           margin: 'lg',
+    //           spacing: 'sm',
+    //           contents: [
+    //             {
+    //               type: 'box',
+    //               layout: 'baseline',
+    //               spacing: 'sm',
+    //               contents: [
+    //                 {
+    //                   type: 'text',
+    //                   text: 'Place',
+    //                   color: '#aaaaaa',
+    //                   size: 'sm',
+    //                   flex: 1,
+    //                 },
+    //                 {
+    //                   type: 'text',
+    //                   text: place,
+    //                   wrap: true,
+    //                   color: '#666666',
+    //                   size: 'sm',
+    //                   flex: 5,
+    //                 },
+    //               ],
+    //             },
+    //             {
+    //               type: 'box',
+    //               layout: 'baseline',
+    //               spacing: 'sm',
+    //               contents: [
+    //                 {
+    //                   type: 'text',
+    //                   text: 'Time',
+    //                   color: '#aaaaaa',
+    //                   size: 'sm',
+    //                   flex: 1,
+    //                 },
+    //                 {
+    //                   type: 'text',
+    //                   text: time,
+    //                   wrap: true,
+    //                   color: '#666666',
+    //                   size: 'sm',
+    //                   flex: 5,
+    //                 },
+    //               ],
+    //             },
+    //           ],
+    //         },
+    //         {
+    //           type: 'text',
+    //           text: text,
+    //           wrap: true,
+    //           margin: 'md',
+    //           size: 'sm',
+    //         },
+    //       ],
+    //     },
+    //     footer: {
+    //       type: 'box',
+    //       layout: 'vertical',
+    //       spacing: 'xxl',
+    //       contents: [
+    //         {
+    //           type: 'box',
+    //           layout: 'vertical',
+    //           contents: [
+    //             {
+    //               type: 'button',
+    //               style: 'link',
+    //               height: 'sm',
+    //               action: {
+    //                 type: 'uri',
+    //                 label: 'OK🍻',
+    //                 uri: `${okURL}?id=${id}&isLiff=${isLiff}`,
+    //               },
+    //               color: '#FFFFFF',
+    //             },
+    //           ],
+    //           backgroundColor: '#3b82f6',
+    //           cornerRadius: 'xl',
+    //           paddingAll: 'md',
+    //         },
+    //         {
+    //           type: 'box',
+    //           layout: 'vertical',
+    //           contents: [
+    //             {
+    //               type: 'button',
+    //               style: 'link',
+    //               height: 'sm',
+    //               action: {
+    //                 type: 'uri',
+    //                 label: 'Hmm...🤔',
+    //                 uri: `${hmURL}?id=${id}&isLiff=${isLiff}`,
+    //               },
+    //               color: '#FFFFFF',
+    //             },
+    //           ],
+    //           backgroundColor: '#fbbf24',
+    //           cornerRadius: 'xl',
+    //           paddingAll: 'md',
+    //         },
+    //         {
+    //           type: 'box',
+    //           layout: 'vertical',
+    //           contents: [
+    //             {
+    //               type: 'button',
+    //               style: 'link',
+    //               height: 'sm',
+    //               action: {
+    //                 type: 'uri',
+    //                 label: 'NG🙅‍♂️',
+    //                 uri: `${ngURL}?id=${id}&isLiff=${isLiff}`,
+    //               },
+    //               color: '#FFFFFF',
+    //             },
+    //           ],
+    //           backgroundColor: '#f87171',
+    //           paddingAll: 'md',
+    //           cornerRadius: 'xl',
+    //         },
+    //         {
+    //           type: 'box',
+    //           layout: 'vertical',
+    //           contents: [
+    //             {
+    //               type: 'button',
+    //               style: 'link',
+    //               height: 'sm',
+    //               action: {
+    //                 type: 'uri',
+    //                 label: '回答確認',
+    //                 uri: `${answerURL}?id=${id}&isLiff=${isLiff}`,
+    //               },
+    //               color: '#000000',
+    //             },
+    //           ],
+    //           backgroundColor: '#dddddd',
+    //           paddingAll: 'md',
+    //           cornerRadius: 'xl',
+    //         },
+    //         {
+    //           type: 'spacer',
+    //           size: 'sm',
+    //         },
+    //       ],
+    //       flex: 0,
+    //     },
+    //   },
+    // },
   ]
 
   return messages
