7d6ed14f962a0432a2d011fa62d2c826d565f023
masa-fullstack <ishida.masahidee@gmail.com>
2021-07-09 16:32:14 +0900

    update env api_key

--- src/utils/apiHeaders.ts
+++ src/utils/apiHeaders.ts
@@ -1,5 +1,5 @@
 import { ApiReqHeaders } from '../types/api/Headers'
 
 export const headers: ApiReqHeaders = {
-  'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY ?? '',
+  'X-API-KEY': process.env.API_KEY ?? '',
 }
